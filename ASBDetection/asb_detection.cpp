195e9be41d9ad2dbe5bb89e357f3899cac4e65b5
David Poetzsch-Heffter <davidpoetzsch@web.de>
2016-07-12 00:51:21 +0200

    auto registering instrumentation pass when shared library is loaded

--- ASBDetection/asb_detection.cpp
+++ ASBDetection/asb_detection.cpp
@@ -1,5 +1,7 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Module.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include "llvm/IR/LegacyPassManager.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Support/CommandLine.h"
 
@@ -45,14 +47,30 @@ namespace TaintAnalysis {
             // the module was not modified -> return false
             return false;*/
         }
+
+        virtual const char* getPassName() const {
+            return "Instrumentation for detection of Address Sensitivity";
+        }
     };
 
 /*    cl::opt<bool> ASBDetection::optionDumpTaint("asb_detection_dump_taint", cl::desc("Dump the statically analysed taint information"));
     cl::opt<bool> ASBDetection::optionCheckTaint("asb_detection_check_taint", cl::desc("Check the static taint but do not instrument"));
     cl::opt<bool> ASBDetection::optionInstrumentOnly("asb_detection_instr_only", cl::desc("Only instrument but don't run static checks"));*/
     cl::opt<int> ASBDetection::optionLogLevel("asb-log-level", cl::desc("Specifiy the log level; everything <= will be logged (0-100)"),
-                                              cl::value_desc("level"), cl::init(10));
+                                              cl::value_desc("level"), cl::init(0));
     
     char ASBDetection::ID = 0;
-    static RegisterPass<ASBDetection> X("asb-detection", "instrument for address sensitivity detection", false, false);
+    static RegisterPass<ASBDetection> X("asb-detection", "Instrumentation for detection of Address Sensitivity", false, false);
 }
+
+// this little magic enables automatic enabling of the pass when it is loaded
+// see http://adriansampson.net/blog/clangpass.html for details
+static void registerMyPass(const PassManagerBuilder &,
+                           legacy::PassManagerBase &PM) {
+    PM.add(new TaintAnalysis::ASBDetection());
+}
+// see http://llvm.org/docs/doxygen/html/classllvm_1_1PassManagerBuilder.html#a575d14758794b0997be4f8edcef7dc91
+// for details on the running order
+static RegisterStandardPasses
+    RegisterMyPass(PassManagerBuilder::EP_OptimizerLast,
+                   registerMyPass);

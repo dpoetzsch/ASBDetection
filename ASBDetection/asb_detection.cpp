0aaf08f308268080c756b8534a74ca75ce5b0656
David Poetzsch-Heffter <davidpoetzsch@web.de>
2016-07-11 23:58:01 +0200

    removed unused options from asb detection instrumentation pass

--- ASBDetection/asb_detection.cpp
+++ ASBDetection/asb_detection.cpp
@@ -13,18 +13,18 @@ using namespace llvm;
 namespace TaintAnalysis {
     struct ASBDetection : public ModulePass {
         static char ID;
-        static cl::opt<bool> optionDumpTaint;
+/*        static cl::opt<bool> optionDumpTaint;
         static cl::opt<bool> optionCheckTaint;
-        static cl::opt<bool> optionInstrumentOnly;
+        static cl::opt<bool> optionInstrumentOnly;*/
         static cl::opt<int> optionLogLevel;
 
         ASBDetection() : ModulePass(ID) {}
 
         virtual bool runOnModule(Module &M) {
-            if (optionInstrumentOnly) {
+//            if (optionInstrumentOnly) {
                 InstrTaintgrindVisitor instrv(optionLogLevel);
                 return instrv.instrumentModule(M);
-            }
+//            }
             
 	    /*            bool taintChanged = true;
             TaintVisitor vis;
@@ -40,19 +40,19 @@ namespace TaintAnalysis {
                 dumper.visit(M);
                 // the module was not modified -> return false
                 return false;
-		}*/
+		}
 
             // the module was not modified -> return false
-            return false;
+            return false;*/
         }
     };
 
-    cl::opt<bool> ASBDetection::optionDumpTaint("asb_detection_dump_taint", cl::desc("Dump the statically analysed taint information"));
+/*    cl::opt<bool> ASBDetection::optionDumpTaint("asb_detection_dump_taint", cl::desc("Dump the statically analysed taint information"));
     cl::opt<bool> ASBDetection::optionCheckTaint("asb_detection_check_taint", cl::desc("Check the static taint but do not instrument"));
-    cl::opt<bool> ASBDetection::optionInstrumentOnly("asb_detection_instr_only", cl::desc("Only instrument but don't run static checks"));
+    cl::opt<bool> ASBDetection::optionInstrumentOnly("asb_detection_instr_only", cl::desc("Only instrument but don't run static checks"));*/
     cl::opt<int> ASBDetection::optionLogLevel("asb-log-level", cl::desc("Specifiy the log level; everything <= will be logged (0-100)"),
                                               cl::value_desc("level"), cl::init(10));
     
     char ASBDetection::ID = 0;
-    static RegisterPass<ASBDetection> X("asb_detection", "test function exist", false, false);
+    static RegisterPass<ASBDetection> X("asb_detection", "instrument for address sensitivity detection", false, false);
 }

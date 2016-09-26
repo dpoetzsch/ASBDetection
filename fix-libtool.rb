36483211626a87ec55f8a0698b5f4b668b70fcae
David Poetzsch-Heffter <davidpoetzsch@web.de>
2016-09-26 21:49:41 +0200

    fixed libtool support

--- fix-libtool.rb
+++ fix-libtool.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
 
-BASEDIR = File.dirname(__FILE__)
+BASEDIR = File.expand_path(File.dirname(__FILE__))
 
 Dir["**/*"].each do |f|
-    next if not File.file?(f)
+    next if not File.file?(f) or not f.end_with?("Makefile")
     
     txt = File.read(f)
     txt2 = txt.gsub(BASEDIR + "/wrappers/libc_wrapper.o", BASEDIR + "/wrappers/libc_wrapper.lo")

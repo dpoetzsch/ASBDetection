0072f16aa4263981c1bd17e40f1b54974eb017d6
David Poetzsch-Heffter <davidpoetzsch@web.de>
2016-10-03 16:01:05 +0200

    fixed setup script

--- setup-ac-build.sh
+++ setup-ac-build.sh
@@ -2,5 +2,6 @@
 
 export ASB_DETECTION_HOME=`realpath $(dirname "$BASH_SOURCE")`
 export CC=clang
+export CXX=clang++
 export CPPFLAGS=" -g -Xclang -load -Xclang $ASB_DETECTION_HOME/ASBDetection/libLLVMasbDetection.so -mllvm -asb-log-level -mllvm 0"
 export LDFLAGS="-g"
